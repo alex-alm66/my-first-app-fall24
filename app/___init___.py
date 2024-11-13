57e315f627f9b77df067033ba140462093cfa1d7
alex-alm66 <afa66@georgetown.edu>
2024-11-13 16:36:27 -0500

    finished test implementation 2-2

--- app/___init___.py
+++ app/___init___.py
@@ -1,1 +1,1 @@
-# the init file helps us import code from the directory where it in this case is the app directory
+# the init file helps us import code from the directory where it exists (in this case the app dir)
